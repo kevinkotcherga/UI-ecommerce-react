fe620811ffb8f4c74de639b339bea65d66e4105c
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-04 18:26:51 +0100

    ✨feat(home): ajout du slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/flagged/photo-1570733117311-d990c3816c47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+		title: "VENTE D'ÉTÉ",
+		desc: 'NE FAITES PAS DE COMPROMIS SUR LE STYLE ! BÉNÉFICIEZ DE 30% DE REDUCTION SUR LES NOUVEAUX ARRIVAGES',
+		bg: 'f5fafd',
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1612731486606-2614b4d74921?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80',
+		title: 'VENTE POPULAIRE',
+		desc: 'NE FAITES PAS DE COMPROMIS SUR LE STYLE ! BÉNÉFICIEZ DE 30% DE REDUCTION SUR LES NOUVEAUX ARRIVAGES',
+		bg: 'fbf0f4',
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1585790812444-fdb3edec78cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+		title: 'FACILE POUR TOUS LES JOURS',
+		desc: 'NE FAITES PAS DE COMPROMIS SUR LE STYLE ! BÉNÉFICIEZ DE 30% DE REDUCTION SUR LES NOUVEAUX ARRIVAGES',
+		bg: 'fcf1ed',
+	},
+];

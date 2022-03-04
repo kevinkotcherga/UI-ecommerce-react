c86bbb93b16938fc4d23c5c52a7fab09cc3a5ecb
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-04 17:20:33 +0100

    ✨feat(home): ajout du composant annoncement

---
+++ src/components/Annoncement.jsx
@@ -0,0 +1,20 @@
+import styledComponents from "styled-components"
+
+const Container = styledComponents.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`
+
+export const Annoncement = () => {
+  return (
+    <Container>
+      Super affaire  ! Livraison gratuite sur les commandes de plus de 50€
+    </Container>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Annoncement } from '../components/Annoncement'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Annoncement />
       <Navbar />
     </div>
   )

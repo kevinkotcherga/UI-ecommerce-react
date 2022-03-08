fbaedfdbbce16541aa78274c58331b71633d319e
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-08 10:53:30 +0100

    ✨feat(register et login):création de la page register et login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,67 @@
 import styled from "styled-components"
 
 const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.5)), url('https://images.unsplash.com/photo-1505340261985-ef955dabca9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80') center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 25%;
+  background-color: white;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+`
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
 `
 
 const Login = () => {
   return (
-    <Container>
-
+     <Container>
+      <Wrapper>
+        <Title>CONNEXION</Title>
+        <Form>
+          <Input placeholder="pseudo" />
+          <Input placeholder="mot de passe" />
+          <Button>CONNEXION</Button>
+          <Link>MOT DE PASSE OUBLIÉ ?</Link>
+          <Link>CRÉER UN NOUVEAU COMPTE</Link>
+        </Form>
+      </Wrapper>
     </Container>
   )
 }
